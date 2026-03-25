f378e6000a6b29b376b7c11e5fd837836b14ae75
Muthana Torsson <muthana.tursson@gmail.com>
2026-03-25 14:30:44 +0200

    Added tire change service subclasses

--- src/main/java/Lexcon/SummerTireChange.java
+++ src/main/java/Lexcon/SummerTireChange.java
@@ -1,4 +1,20 @@
 package Lexcon;
 
-public class SummerTireChange {
+public class SummerTireChange extends Service {
+
+    private double discount;
+
+    public SummerTireChange(int id, String name, double basePrice, double discount) {
+        super(id, name, basePrice);
+        this.discount = discount;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    @Override
+    public double calculatePrice() {
+        return getBasePrice() - discount;
+    }
 }
--- src/main/java/Lexcon/WinterTireChange.java
+++ src/main/java/Lexcon/WinterTireChange.java
@@ -1,4 +1,20 @@
 package Lexcon;
 
-public class WinterTireChange {
+public class WinterTireChange extends Service {
+
+    private double storageFee;
+
+    public WinterTireChange(int id, String name, double basePrice, double storageFee) {
+        super(id, name, basePrice);
+        this.storageFee = storageFee;
+    }
+
+    public double getStorageFee() {
+        return storageFee;
+    }
+
+    @Override
+    public double calculatePrice() {
+        return getBasePrice() + storageFee;
+    }
 }

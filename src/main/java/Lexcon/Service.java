d79113282c92001873d35eb6b52046dc49d3687f
Muthana Torsson <muthana.tursson@gmail.com>
2026-03-25 12:08:04 +0200

    Added abstract Service class with fields and method

--- src/main/java/Lexcon/Service.java
+++ src/main/java/Lexcon/Service.java
@@ -1,4 +1,28 @@
 package Lexcon;
 
-public class Service {
+public abstract class Service {
+
+    private int id;
+    private String name;
+    private double basePrice;
+
+    public Service(int id, String name, double basePrice) {
+        this.id = id;
+        this.name = name;
+        this.basePrice = basePrice;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBasePrice() {
+        return basePrice;
+    }
+
+    public abstract double calculatePrice();
 }

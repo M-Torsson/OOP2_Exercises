1f50ce12b67c0aa57a4f9cf4f5ec630bb200802b
Muthana Torsson <muthana.tursson@gmail.com>
2026-03-25 15:30:37 +0200

    Added booking and booking request classes

--- src/main/java/Lexcon/Booking.java
+++ src/main/java/Lexcon/Booking.java
@@ -1,4 +1,26 @@
 package Lexcon;
 
 public class Booking {
+
+    private String customerName;
+    private Service service;
+    private double finalPrice;
+
+    public Booking(String customerName, Service service, double finalPrice) {
+        this.customerName = customerName;
+        this.service = service;
+        this.finalPrice = finalPrice;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public Service getService() {
+        return service;
+    }
+
+    public double getFinalPrice() {
+        return finalPrice;
+    }
 }
--- src/main/java/Lexcon/BookingRequest.java
+++ src/main/java/Lexcon/BookingRequest.java
@@ -1,4 +1,20 @@
 package Lexcon;
 
 public class BookingRequest {
+
+    private String customerName;
+    private Service service;
+
+    public BookingRequest(String customerName, Service service) {
+        this.customerName = customerName;
+        this.service = service;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public Service getService() {
+        return service;
+    }
 }
